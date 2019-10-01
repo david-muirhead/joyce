0114e84cc51e08796ead875ec6fb1904253501b2
David Muirhead <dm@oneiota.com>
2019-10-01 15:39:00 +1000

    updates on things that make me frustrated

--- template-parts/home-sketch-video.php
+++ template-parts/home-sketch-video.php
@@ -1,4 +1,4 @@
 <?php ?>
-	<video id="target_video" loop muted playsinline src="<?php the_sub_field('sketch_video_mp4'); ?>" type="video/mp4" poster="<?=$url?>">
+	<video id="target_video" class="sketchvid"loop muted playsinline src="<?php the_sub_field('sketch_video_mp4'); ?>" type="video/mp4" poster="<?=$url?>">
 	</video>
 <?php  ?>
