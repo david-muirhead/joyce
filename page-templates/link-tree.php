3a328e2a7155b2e54107c9c6b610ed2c19919c23
David Muirhead <dm@oneiota.com>
2020-01-22 18:36:37 +1000

    update

--- page-templates/link-tree.php
+++ page-templates/link-tree.php
@@ -6,14 +6,16 @@ get_header(); ?>
 <div class="link-tree">
 	<span class="about-img noclick" data-bg="url(<?php echo the_field('image', 'option'); ?>)">
 	</span>
-	<div class="list">
+	<ul class="list">
 		<?php
 			if( have_rows('linktree-links','options') ){
 				// loop through the rows of data
 				get_template_part( 'template-parts/linktree');
 			}
 		?>
-	</div>
-	<a href="/">Visit full website</a>
+	</ul>
+	<span class="weblink">
+		<a href="/">Visit full website</a>
+	</span>
 </div>
 <?php get_footer();
--- template-parts/linktree.php
+++ template-parts/linktree.php
@@ -14,13 +14,9 @@ if( have_rows('linktree-links', 'options') ): ?>
 				$link_title = $link['title'];
 				$link_target = $link['target'] ? $link['target'] : '_self';
 				?>
+				<li><a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><span><?php echo esc_html($link_title); ?></span><p>→</p></a></span></li>
 
-					<a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><span><?php echo esc_html($link_title); ?></span><p>→</p></a>
-
-				</span>
 			<?php endif; ?>
-
-
-
+			
 	<?php endwhile; // while( has_sub_field('to-do_lists') ): ?>
 <?php endif; // if( get_field('to-do_lists') ): ?>
