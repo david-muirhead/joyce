97560f150e4a25940958a88556e81f0d4aff08fe
David Muirhead <dm@oneiota.com>
2020-01-23 13:42:56 +1000

    update

--- template-parts/video-external.php
+++ template-parts/video-external.php
@@ -3,7 +3,7 @@
 		<?php if ( get_sub_field( 'custom_player' ) ): ?>
 			<div class="plyr__video-embed" id="player">
 		<?php else: // field_name returned false ?>
-			<div class="plyr__video-embed" >
+			<div class="swag" id="player">
 		<?php endif; // end of if field_name logic ?>
 		<?php
 
