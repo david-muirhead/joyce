f924e5fca16d91ed6d11f9158015592233893f4a
David Muirhead <dm@oneiota.com>
2020-02-03 15:41:30 +1000

    test search

--- template-parts/credits.php
+++ template-parts/credits.php
@@ -3,19 +3,20 @@
 	<input type="checkbox" name="love" value="love" id="love" checked>
 
 	<label for="love"><h5 class="credits-title">Credits</h5><i></i></label>
+	<div class="credits-inner-wrap">
+		<ul class="credits-inner">
+			<?php while( have_rows('credits') ): the_row();
 
-	<ul class="credits-inner">
-	<?php while( have_rows('credits') ): the_row();
-
-		// vars
-		$title = get_sub_field('title');
-		$name = get_sub_field('name');
-		?>
-		<li >
-			<h5><?php echo $title; ?></h5>
-			<hr>
-			<p><?php echo $name; ?></p>
-		</li>
-	<?php endwhile; ?>
-</ul>
+				// vars
+				$title = get_sub_field('title');
+				$name = get_sub_field('name');
+				?>
+				<li >
+					<h5><?php echo $title; ?></h5>
+					<hr>
+					<p><?php echo $name; ?></p>
+				</li>
+			<?php endwhile; ?>
+		</ul>
+	</div>
 </div>
