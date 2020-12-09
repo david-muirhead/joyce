f76394f80433496e17b2f65b655c597eaccf739a
David Muirhead <davidmuirhead@Davids-MacBook-Pro.local>
2020-12-09 13:12:08 +1000

    arrow width

--- template-parts/linktree.php
+++ template-parts/linktree.php
@@ -14,7 +14,7 @@ if( have_rows('linktree-links', 'options') ): ?>
 				$link_title = $link['title'];
 				$link_target = $link['target'] ? $link['target'] : '_self';
 				?>
-				<li><a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><span><?php echo esc_html($link_title); ?></span><p>→</p></a></span></li>
+				<li><a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><span><?php echo esc_html($link_title); ?><p>→</p></span></a></span></li>
 
 			<?php endif; ?>
 			
